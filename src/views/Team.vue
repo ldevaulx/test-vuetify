af75f8f2cec0d8146ca816ccec645432b6f398fa
Laurent <ldevaulx@hotmail.com>
2020-01-24 18:04:48 +0100

    wip

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,7 +2,25 @@
     <div class="team">
         <h1 class="subtitle-1">Team</h1>
         <v-container class="my-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur dignissimos dolorum eos facere fugit laboriosam, magni maiores, molestias, nam neque nesciunt nobis omnis provident quisquam reiciendis sit soluta unde?</p>
+            <v-row>
+                <v-col cols="12" sm="6" md="4" lg="3" v-for="person in team" :key="person.name">
+                    <v-card flat class="text-center ma-3">
+                        <v-responsive class="pt-4">
+                            image here
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="subheading">{{ person.name }}</div>
+                            <div class="grey--text">{{ person.role }}</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn text color="grey">
+                                <v-icon small left>mdi-message</v-icon>
+                                <span class="">Message</span>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
@@ -10,7 +28,15 @@
 <script>
     export default {
         data() {
-            return { }
+            return {
+                team: [
+                    { name: 'The Net Ninja', role: 'Web developer' },
+                    { name: 'Ryu', role: 'Graphic designer' },
+                    { name: 'Chun Li', role: 'Web developer' },
+                    { name: 'Gouken', role: 'Social media maverick' },
+                    { name: 'Yoshi', role: 'Sales guru'}
+                ]
+            }
         }
     }
 </script>
