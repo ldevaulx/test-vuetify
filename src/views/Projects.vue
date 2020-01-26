e0614985294ee00d87057aba9ef059601ecb3e38
Laurent de Vaulx <ldevaulx@hotmail.com>
2020-01-26 21:29:21 +0100

    wip

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,17 @@
     <div class="projects">
         <h1 class="subtitle-1">Projects</h1>
         <v-container class="my-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur dignissimos dolorum eos facere fugit laboriosam, magni maiores, molestias, nam neque nesciunt nobis omnis provident quisquam reiciendis sit soluta unde?</p>
+            <v-expansion-panels hover>
+                <v-expansion-panel
+                        v-for="(item,i) in 5"
+                        :key="i"
+                >
+                    <v-expansion-panel-header>Item</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-container>
     </div>
 </template>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="team">
         <h1 class="subtitle-1">Team</h1>
+        <v-avatar color="red">
+            <span>TT</span>
+        </v-avatar>
         <v-container class="my-5">
             <v-row>
                 <v-col cols="12" sm="6" md="4" lg="3" v-for="person in team" :key="person.name">
                     <v-card flat class="text-center ma-3">
-                        <v-responsive class="pt-4">
-                            image here
-                        </v-responsive>
+                        <div></div>
+                        <v-avatar color="blue">
+                            <img :src="person.avatar">
+                        </v-avatar>
                         <v-card-text>
                             <div class="subheading">{{ person.name }}</div>
                             <div class="grey--text">{{ person.role }}</div>
@@ -30,11 +34,11 @@
         data() {
             return {
                 team: [
-                    { name: 'The Net Ninja', role: 'Web developer' },
-                    { name: 'Ryu', role: 'Graphic designer' },
-                    { name: 'Chun Li', role: 'Web developer' },
-                    { name: 'Gouken', role: 'Social media maverick' },
-                    { name: 'Yoshi', role: 'Sales guru'}
+                    { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+                    { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+                    { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+                    { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+                    { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
                 ]
             }
         }
